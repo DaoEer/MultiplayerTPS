90fc2ca13ce099e184bd5c759d327050fbbc08d8
DaoEer <daoeer@qq.com>
2025-08-03 18:13:31 +0800

    Project Settings

--- Source/MultiplayerTPS/MultiplayerTPS.Build.cs
+++ Source/MultiplayerTPS/MultiplayerTPS.Build.cs
@@ -7,14 +7,14 @@ public class MultiplayerTPS : ModuleRules
 	public MultiplayerTPS(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "MultiplayerSessions" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "OnlineSubsystem", "MultiplayerSessions" });
+
+		PrivateDependencyModuleNames.AddRange(new string[] { });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
+
 		// Uncomment if you are using online features
 		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
 
